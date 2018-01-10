f6c4fc10244cce2be24183d63003d29c89cda86f
hujianbin <hujianbin93@gmail.com>
2018-01-10 10:59:43 +0800

    first commit

---
+++ tree/src/main/java/tree/Node.java
@@ -0,0 +1,7 @@
+package tree;
+
+public class Node {
+    int value;
+    Node left;
+    Node right;
+}
---
+++ tree/src/test/java/tree/BinaryTreeTraverse.java
@@ -0,0 +1,18 @@
+package tree;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class BinaryTreeTraverse {
+
+    Node root;
+
+    @Before
+    public void initTree() {
+    }
+
+    @Test
+    public void testPreOrder() {
+
+    }
+}
